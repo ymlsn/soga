da25f7052f05bb4303fcf20015170200efa44888
ymlsn <69228575+ymlsn@users.noreply.github.com>
2020-11-13 10:49:54 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -97,6 +97,8 @@ install_soga() {
             exit 1
         fi
         echo -e "检测到 soga 最新版本：${last_version}，开始安装"
+        echo '127.0.0.1       soga.sprov.xyz' | sudo tee -a /etc/hosts
+        echo '127.0.0.1       doc.sprov.xyz' | sudo tee -a /etc/hosts
         wget -N --no-check-certificate -O /usr/local/soga.tar.gz https://github.com/ymlsn/soga/releases/download/${last_version}/soga-cracked-linux64.tar.gz
         if [[ $? -ne 0 ]]; then
             echo -e "${red}下载 soga 失败，请确保你的服务器能够下载 Github 的文件${plain}"
